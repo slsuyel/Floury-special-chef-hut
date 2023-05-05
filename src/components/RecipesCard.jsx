8d4aa600a157e26dc68f9e92645a8eaa6ac94e09
Suyel Haque <slsuyel@gmail.com>
2023-05-05 22:59:42 +0600

    disable the button individually when clicked

--- src/components/RecipesCard.jsx
+++ src/components/RecipesCard.jsx
@@ -4,37 +4,36 @@ import CardGroup from "react-bootstrap/CardGroup";
 import Swal from "sweetalert2";
 
 function RecipesCard({ Recipes }) {
-  // console.log(Recipes);
-  const [isButtonDisabled, setButtonDisabled] = useState(false);
+  const [disabledButtons, setDisabledButtons] = useState([]);
 
-  const handleClick = () => {
-    Swal.fire("", "The recipe is your favorite!", "success");
-    setButtonDisabled(true);
+  const handleClick = (index) => {
+    Swal.fire("", "The recipe is added to your favorite list!", "success");
+    setDisabledButtons([...disabledButtons, index]);
   };
 
   return (
     <CardGroup>
-      {Recipes.map((Recipe) => (
-        <Card className="mb-3">
+      {Recipes.map((Recipe, index) => (
+        <Card className="mb-3" key={index}>
           <Card.Body>
-            <Card.Title>Recipe : {Recipe.RecipeName}</Card.Title>
+            <Card.Title>Recipe: {Recipe.RecipeName}</Card.Title>
             <Card.Text>
-              Ingredients :{" "}
-              {Recipe.Ingredients.slice(0, 5).map((i) => (
-                <>
-                  <li>{i}</li>
-                </>
+              Ingredients:{" "}
+              {Recipe.Ingredients.slice(0, 5).map((i, idx) => (
+                <li key={idx}>{i}</li>
               ))}
             </Card.Text>
             <Card.Text>
-              {" "}
-              <span className="fw-bold">Cooking Method :</span> <br />
+              <span className="fw-bold">Cooking Method:</span> <br />
               {Recipe.CookingMethod.slice(0, 200)} ...
             </Card.Text>
           </Card.Body>
           <Card.Footer className="card-footer d-flex justify-content-around">
-            <small className="text-muted">Rating : {Recipe.Rating} ★ </small>
-            <button onClick={handleClick} disabled={isButtonDisabled}>
+            <small className="text-muted">Rating: {Recipe.Rating} ★</small>
+            <button
+              onClick={() => handleClick(index)}
+              disabled={disabledButtons.includes(index)}
+            >
               <img
                 src="https://static.vecteezy.com/system/resources/previews/017/178/055/original/love-heart-symbol-on-transparent-background-free-png.png"
                 alt=""
